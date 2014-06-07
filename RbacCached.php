b8af188aa14bbb84fc8e720c48492d677658b7bc
Ngua Go <nguago@let.vn>
2014-06-07 15:51:50 +0700

    [Update] configure Cache component

--- RbacCached.php
+++ RbacCached.php
@@ -11,23 +11,30 @@ namespace letyii\rbaccached;
 
 use Yii;
 use yii\rbac\DbManager;
+use yii\helpers\ArrayHelper;
 
 class RbacCached extends DbManager {
 
     /**
-     * @var integer Lifetime of cached data in seconds
-     */
-    public $lifetime = 3600;
-
-    /**
-     * @var string cache prefix to ovoid collisions
-     */
-    public $cachePrefix = 'RbacCached_';
-    /**
      * @var string the ID of the cache application component that is used to cache rbac.
      * Defaults to 'cache' which refers to the primary cache application component.
      */
-    public $cacheId = 'cache';
+    public $cache = 'cache';
+
+    /**
+     * @var integer Lifetime of cached data in seconds
+     */
+    public $cacheDuration = 3600;
+
+    /**
+     * @var string cache key name
+     */
+    public $cacheKeyName = 'RbacCached';
+
+    /**
+     * @var array php cache
+     */
+    protected $cachedData = [];
 
     /**
      * @inheritdoc
@@ -43,9 +50,9 @@ class RbacCached extends DbManager {
         if (!empty($params))
             return parent::checkAccess($userId, $permissionName, $params);
 
-        $cacheKey = $this->cachePrefix . 'checkAccess:' . $userId . ':' . $permissionName;
+        $cacheKey = 'checkAccess:' . $userId . ':' . $permissionName;
         $cached = $this->getCache($cacheKey);
-        if ($cached === FALSE) {
+        if (empty($cached)) {
             $cached = parent::checkAccess($userId, $permissionName);
             $this->setCache($cacheKey, $cached);
         }
@@ -56,12 +63,12 @@ class RbacCached extends DbManager {
      * @inheritdoc
      */
     protected function checkAccessRecursive($user, $itemName, $params, $assignments) {
-        $cacheKey = $this->cachePrefix . 'checkAccessRecursive:' . $user . ':' . $itemName;
+        $cacheKey = 'checkAccessRecursive:' . $user . ':' . $itemName;
         if (!empty($params))
             $cacheKey .= ':' . current($params)->primaryKey;
 
         $cached = $this->getCache($cacheKey);
-        if ($cached === FALSE) {
+        if (empty($cached)) {
             $cached = parent::checkAccessRecursive($user, $itemName, $params, $assignments);
             $this->setCache($cacheKey, $cached);
         }
@@ -72,9 +79,9 @@ class RbacCached extends DbManager {
      * @inheritdoc
      */
     protected function getItem($name) {
-        $cacheKey = $this->cachePrefix . 'Item:' . $name;
+        $cacheKey = 'Item:' . $name;
         $cached = $this->getCache($cacheKey);
-        if ($cached === FALSE) {
+        if (empty($cached)) {
             $cached = parent::getItem($name);
             $this->setCache($cacheKey, $cached);
         }
@@ -85,9 +92,9 @@ class RbacCached extends DbManager {
      * @inheritdoc
      */
     public function getAssignments($userId) {
-        $cacheKey = $this->cachePrefix . 'Assignments:' . $userId;
+        $cacheKey = 'Assignments:' . $userId;
         $cached = $this->getCache($cacheKey);
-        if ($cached === FALSE) {
+        if (empty($cached)) {
             $cached = parent::getAssignments($userId);
             $this->setCache($cacheKey, $cached);
         }
@@ -100,10 +107,10 @@ class RbacCached extends DbManager {
      * @param $value
      * @return mixed
      */
-    protected function setCache($key, $value)
-    {
-        $cacheComponent  = $this->resolveCacheComponent();
-        return $cacheComponent->set($key, $value, $this->lifetime);
+    protected function setCache($key, $value) {
+        $this->cachedData = $this->resolveCacheComponent()->get($this->cacheKeyName);
+        $this->cachedData[$key] = $value;
+        return $this->resolveCacheComponent()->set($this->cacheKeyName, $this->cachedData, $this->cacheDuration);
     }
 
     /**
@@ -111,18 +118,21 @@ class RbacCached extends DbManager {
      * @param $key
      * @return mixed
      */
-    protected function getCache($key)
-    {
-        $cacheComponent = $this->resolveCacheComponent();
-        return $cacheComponent->get($key);
+    protected function getCache($key) {
+        $cached = ArrayHelper::getColumn($this->cachedData, $key);
+        if (!isset($cached)) {
+            $cacheData = $this->resolveCacheComponent()->get($this->cacheKeyName);
+            $cached = $this->cachedData[$key] = ArrayHelper::getColumn($cacheData, $key);
+        }
+        return $cached;
     }
 
     /**
      * Returns cache component configured as in cacheId
      * @return Cache
      */
-    protected function resolveCacheComponent()
-    {
-        return Yii::$app->get($this->cacheId);
+    protected function resolveCacheComponent() {
+        return Yii::$app->get($this->cache);
     }
+
 }
